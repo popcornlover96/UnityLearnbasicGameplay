7845e7b90c806542cfe7fcbd09c665a080d2cb06
Jacob <victorj@roverkids.org>
2026-04-27 13:21:26 -0400

    Finished 2.2

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,10 +19,20 @@ public class PlayerController : MonoBehaviour
     {
       // Keep the player in bounds 
         if (transform.position.x < xRange)
-        { transform.position = new Vector3( -xRange,transform.position.y, transform.position.z); }
+        { transform.position = new Vector3( xRange,transform.position.y, transform.position.z); }
+
         if (transform.position.x > xRange)
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-            horizontalInput = Input.GetAxis("horizontal"); 
+        { transform.position = new Vector3(xRange, transform.position.y, transform.position.z); }
+           
+        horizontalInput = Input.GetAxis("Horizontal"); 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
+
+    
 }
